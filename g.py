cd8804deb9ca9e0c03898d8efa523fede261f09b
omarv7y <70105391+omarv7y@users.noreply.github.com>
2021-02-03 18:55:32 +0200

    Update g.py

--- g.py
+++ g.py
@@ -1,6 +1,6 @@
 def i ():
 	k = input ('Nam: ')
-	if k == '1':                                              
+	if k == '4':                                              
 		print ('\033[1;32m omar oh oh oh')
 	else:
         	print ('\033[1;31m aff ')
